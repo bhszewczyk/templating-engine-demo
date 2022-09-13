1a63e0edb98142d8f0345a22917667c51db55e25
Barbara Szewczyk <comamysza@gmail.com>
2022-09-13 22:56:45 +0200

    Sets views directory

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // adding EJS (first install this)
 // npm i ejs
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000...');
