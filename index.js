84fbd55c3353d274ad3ed02b69ea1caddedc34d3
Barbara Szewczyk <comamysza@gmail.com>
2022-09-15 14:42:51 +0200

    Adds looping to display posts

--- index.js
+++ index.js
@@ -8,29 +8,33 @@ const subpageData = require('./data.json');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
-app.listen(3000, () => {
-	console.log('Listening on port 3000...');
-});
-
 app.get('/', (req, res) => {
 	// by default EJS looks for a folder /views
 	// no need to add .ejs
 	res.render('home');
 });
+app.get('/s/:subpage', (req, res) => {
+	const { subpage } = req.params;
+	const data = subpageData[subpage.toLowerCase()];
+	if (data) {
+		res.render('subpage', { ...data });
+	} else {
+		return;
+	}
+});
 
-app.get('/dogs', (req, res) => {
+app.get('/s/dogs', (req, res) => {
 	const dogs = ['Eevee', 'Ponyo', 'Volo', 'Ida'];
 
 	res.render('dogs', { dogs });
 });
 
-app.get('/rand', (req, res) => {
+app.get('/s/rand', (req, res) => {
 	// defining variables for EJS
 	const num = Math.floor(Math.random() * 10) + 1;
 	res.render('random', { rand: num });
 });
 
-app.get('/:subpage', (req, res) => {
-	const { subpage } = req.params;
-	res.render('subpage', { subpage: subpage });
+app.listen(3000, () => {
+	console.log('Listening on port 3000...');
 });
